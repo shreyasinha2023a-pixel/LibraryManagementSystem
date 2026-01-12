5a5b220916017d2b74cfb378ac0f00c7be4078e3
Shreya Sinha <shreya22sinha23@gmail.com>
2026-01-12 22:51:07 +0530

    Dev A added issue book function

--- library.py
+++ library.py
@@ -3,8 +3,9 @@ def register_student(name):
     print(f"{name} registered successfully")
 def login(username, password):
     print("Login successful")
-# Library Management System - Dev B Branch
 def issue_book(book_name):
-    print(f"{book_name} issued to student")
+    print(f"{book_name} issued successfully")
+
+
 
 
