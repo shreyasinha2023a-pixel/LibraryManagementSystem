19961d7492694b65acb55498db3ec5dc73cdfe41
Shreya Sinha <shreya22sinha23@gmail.com>
2026-01-12 22:47:24 +0530

    Modified book issue feature by Dev B

--- library.py
+++ library.py
@@ -3,6 +3,8 @@ def register_student(name):
     print(f"{name} registered successfully")
 def login(username, password):
     print("Login successful")
-# Library Management System - Main Branch
+# Library Management System - Dev B Branch
 def issue_book(book_name):
-    print(f"{book_name} issued successfully")
+    print(f"{book_name} issued to student")
+
+
